d9b27837e618448e9fe2b7c23b9bfb895bdd324c
AspetDavoodi <48726573+AspetDavoodi@users.noreply.github.com>
2019-04-15 01:13:32 +0400

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,47 @@ import json
 
 def main ():
 
+    def getRollData():
+        with open('RollData.json', 'r') as fp:
+            global RollData
+            RollData = json.load (fp)
+
+
     def userMode ():
         pass
 
 
     def adminMode ():
-        pass
+        print("""
+        Hello! You are now in Admin mode where you can add new rolls of film that you found are missing from the database,
+        Here are the list of Brands that you can add to.""")
+        print ("""
+        1-Kodak
+        2-FujiColor
+        3-Lomography
+        4-Cinestill
+        5-Rollei
+        6-Ilford
+        7-Kentmere
+        8-Foma
+        9-Dubblefilm
+        10-Streetcandy
+        11-JCH
+        12-Kosmophoto
+        13-Santa
+        14-Adox
+        15-Revolog
+        """)
+
+        BrandChoice = int (input ("Please select the number of the corresponding brand that you'd like to add a roll to\n"))
+
+    getRollData()
 
     modeChoice = int (input ("""
     Please choose operation mode\n
     1- for Admin mode (to add new rolls top database)\n
-    2- for User mode """))
+    2- for User mode 
+    """))
 
     while True:
         if modeChoice == 1:
