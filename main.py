cc8dcf226384dc362d27fb24425a511bef4ed4d2
AspetDavoodi <48726573+AspetDavoodi@users.noreply.github.com>
2019-05-16 16:34:34 +0400

    Added User mode functionality

--- main.py
+++ main.py
@@ -1,198 +1,421 @@
 import json
 
-def main ():
+def checkAgreement(argument):
+    yesWords = ("Yes", "yes", "YES", "YeS", "yEs","yus","Yus","Yup","yup","mhm")
+
+    noWords = ("No", "no", "nO", "nope", "nein","nou","neh","nah", "Nein","Nou","Neh","Nah")
+
+    while True:
+        if argument in yesWords:
+            output = "1"
+            break
+        elif argument in noWords:
+            output = "0"
+            break
+        else:
+            output = "2"
+            break
+    return output
+
+def getRollData():
+    with open('RollData.json', 'r') as rolldata:
+        RollData = json.load (rolldata)
+    return RollData
+
+def modelsInBrand(Brand, RollData):
+    for key in RollData[Brand]:
+        print(key['model'])
+
+def modeChoice(RollData):
 
 
-    def getRollData():
-        with open('RollData.json', 'r') as rolldata:
-            global RollData
-            RollData = json.load (rolldata)
-        return RollData
-
-    def modelsInBrand(Brand):
-        for key in RollData[Brand]:
-            print(key['model'])
-
-    def modeChoice():
-            modeChoice = int(input("""
+        while True:
+            modeChoice = input("""
 Please choose operation mode\n
 1- for Admin mode (to add new rolls top database)\n
 2- for User mode 
 
-input:  """))
+input:  """)
 
-            if modeChoice == 1:
-                while True:
-                    modeContinue = int(input("Would you like to add another roll? If yes, input 1 otherwise input 0 to exit"))
-                    if modeContinue == 1:
-                        adminMode()
-                    elif modeContinue == 0:
-                        print("successfully ended admin mode. Bye Bye!")
-                        break
-                    else:
-                        print("Invalid input, please refer to the instructions above.")
+            if modeChoice.isnumeric() and int(modeChoice) == 1:
+               while True:
+                   modeContinue = input("Would you like to add a roll?")
 
-            elif modeChoice == 2:
-                userMode()
+                   modeContinue = int(checkAgreement(modeContinue))
+
+                   if modeContinue == 1:
+                       adminMode(RollData)
+                   elif modeContinue == 0:
+                       print("successfully ended admin mode. Bye Bye!")
+                       exit()
+                   else:
+                       print("Invalid input, please try again")
+            elif modeChoice.isnumeric() and int(modeChoice) == 2:
+                userMode(RollData)
+                break
             else:
                 print("invalid choice, please follow the instructions for choosing mode of operation")
 
-    def userMode ():
-        pass
+def userMode (RollData):
 
-    def adminMode ():
+    print("""Hello! You are now in User Mode!
 
-        def brandChoice():
+This program will help you choose a roll of film based on:
 
-            while True:
-                tempBrandChoice = (input("\nPlease input the name of the Brand you would like to add a roll to.\n"))
+-Your budget.
+-The time of day during which you are going to shoot the most.
+-Choice of black and white or color.
+-developing Processes available to you.
 
-                brandValidation = RollData.get(tempBrandChoice)
+""")
 
-                if brandValidation is None:
-                    print("Invalid input, please input the name of the brand you want to select")
-                else:
-                    break
-            return tempBrandChoice
+    def getBudget():
+        while True:
+
+            userInput = input("Are you willing to pay more than 7 USD for a roll of film?")
+            userInput = int (checkAgreement(userInput))
+
+            if userInput == 1:
+                is_Highend = True
+                break
+            elif userInput == 0:
+                is_Highend = False
+                break
+            else: print("Invalid input, please try again")
+
+        return is_Highend
 
 
-        def ISOWrite():
-            ISORange = int(input("How many ISO's is the roll available in? (input an integer number)"))
-            ISOValues = []
-            for i in range(ISORange):
-                ISO = input("Please write the ISO values of the roll, in increasing order")
-                ISOValues.append(int(ISO))
-            return ISOValues
+    def getTimeofDay():
+        morningChoice = ("morning", "Morning")
+        noonChoice = ("noon", "Noon")
+        eveningChoice = ("evening", "Evening")
+        nightChoice = ("night", "Night")
 
-        def typeWrite():
+        while True:
+            time = input("""at what time during the day are you going to shoot the most?
+Here's how to input:
 
-            rollType = int(input("""
-Is the film color or black and white?-
-input 0 for Black and White
-input 1 for color
-input:  """))
+08 am to 12 pm - write morning
+12 pm to 15 pm - write noon
+15 pm to 18 pm - write evening
+after 18 pm - write night
+""")
 
-            while True:
-                if rollType == 0:
-                    is_Color = False
-                    break
-                elif rollType == 1:
-                    is_Color = True
-                    break
-                else:
-                    print ("Invalid input! please refer to instructions above to select film type")
-            return is_Color
+            if time in morningChoice:
+                timeofday = 1
+                break
+            elif time in noonChoice:
+                timeofday = 2
+                break
+            elif time in eveningChoice:
+                timeofday = 3
+                break
+            elif time in nightChoice:
+                timeofday = 4
+                break
+            else: print("Invalid input, please try again")
 
-        def usageWrite():
-            usageValues = []
-            while True:
-                usage = input("""
-Please write the names of any of the following types of photography, where this roll can be used:
+        return timeofday
+
+
+    def timeofDaytoISO(timeofday):
+
+        ISOmin = 0
+        ISOmax = 0
+
+        if timeofday == 1:
+            ISOmin = 0
+            ISOmax = 200
+
+        elif timeofday == 2:
+            ISOmin = 200
+            ISOmax = 400
+
+        elif timeofday == 3:
+            ISOmin = 400
+            ISOmax = 800
+
+        elif timeofday == 4:
+            ISOmin = 800
+            ISOmax = 6400
+
+        ISO = range(ISOmin, ISOmax)
+        return ISO
+
+
+    def getColor():
+        while True:
+            userInput = input("Do you want color film?")
+            userInput = int(checkAgreement(userInput))
+
+            if userInput == 1:
+                is_Color = True
+                break
+            elif userInput == 0:
+                is_Color = False
+                break
+            else: print("Invalid input, please try again")
+        return is_Color
+
+
+    def getProcess():
+
+        processes = []
+
+        while True:
+            C41userInput = input("Do you have access to C-41 (Color Negative) development?")
+
+            C41userInput = int(checkAgreement(C41userInput))
+
+            if C41userInput == 1:
+                processes.append("Negative")
+                break
+            elif C41userInput == 0:
+                pass
+                break
+            else: print("Invalid input, Try again")
+
+        while True:
+            E6userInput = input("Do you have access to E-6 (Color Reversal- Slide) development?")
+            E6userInput = int(checkAgreement(E6userInput))
+
+            if E6userInput == 1:
+                processes.append("Slide")
+                break
+            elif E6userInput == 0:
+                pass
+                break
+            else: print("Invalid input, Try again")
+
+        return processes
+
+
+    def findRolls(RollData, ISO, Budget, Color, Process=["N/A"]):
+
+        brands = []
+        ISOfilter = []
+        BudgetFilter = []
+        colorFilter = []
+        processFilter = []
+
+
+        for i in RollData:
+            brands.append(i)
+
+        for i in brands:
+            for c in RollData[i]:
+                for z in c["ISO"]:
+                    if z in ISO:
+                        ISOfilter.append(c["model"])
+
+        for i in brands:
+            for c in RollData[i]:
+                if c["is_HighEnd"] == Budget:
+                    BudgetFilter.append(c["model"])
+
+        for i in brands:
+            for c in RollData[i]:
+                if c["is_Color"] == Color:
+                    colorFilter.append(c["model"])
+
+        for i in brands:
+            for c in RollData[i]:
+                if c["type"] in Process:
+                    processFilter.append(c["model"])
+
+
+        ISOfilter = set(ISOfilter)
+        BudgetFilter = set(BudgetFilter)
+        colorFilter = set(colorFilter)
+        processFilter = set(processFilter)
+
+        Filtered1 = ISOfilter.intersection(BudgetFilter)
+        Filtered2 = colorFilter.intersection(processFilter)
+
+        TotalFilter = Filtered1.intersection(Filtered2)
+
+        return (TotalFilter)
+
+    is_Highend = getBudget()
+    is_Color = getColor()
+    timeofday = getTimeofDay()
+    ISO = timeofDaytoISO(timeofday)
+    Process = getProcess()
+
+
+    FoundRolls = findRolls(RollData, ISO, is_Highend, is_Color, Process)
+
+    print(FoundRolls)
+
+def adminMode (RollData):
+
+    def brandChoice():
+
+        while True:
+            tempBrandChoice = (input("\nPlease input the name of the Brand you would like to add a roll to.\n"))
+
+            brandValidation = RollData.get(tempBrandChoice)
+
+            if brandValidation is None:
+                print("Invalid input, please input the name of the brand you want to select")
+            else:
+                break
+        return tempBrandChoice
+
+    def ISOWrite():
+        ISORange = int(input("How many ISO's is the roll available in? (input an integer number)"))
+        ISOValues = []
+        for i in range(ISORange):
+            ISO = input("Please write the ISO values of the roll, in increasing order")
+            ISOValues.append(int(ISO))
+        return ISOValues
+
+    def typeWrite():
+
+
+        while True:
+
+            rollType = input("""
+Is the film Color? 
+input:  """)
+            rollType = checkAgreement(rollType)
+
+            if rollType.isnumeric() and int(rollType) == 0:
+                is_Color = False
+                break
+            elif rollType.isnumeric() and int(rollType) == 1:
+                is_Color = True
+                break
+            else:
+                print ("Invalid input! please refer to instructions above to select film type")
+        return is_Color
+
+    def usageWrite():
+        usageValues = []
+        while True:
+            usage = input("""
+Please write applications for this film, for example:
 Portraits
 Street
 Landscape
 General
 Studio
-or input 0 to end this process
+
+Afterwards input 0 to end this process
+
 input:  """)
-                if usage.isnumeric() and int(usage) == 0:
-                    break
-                else:
-                    usageValues.append(usage)
+            if usage.isnumeric() and int(usage) == 0:
+                break
+            else:
+                usageValues.append(usage)
 
-            return usageValues
+        return usageValues
 
-        def highEndWrite():
-            highEndArgument = int (input ("Does one roll of the film cost more than 7 USD? If Yes, input 1, if No input 0"))
-            while True:
-                if highEndArgument == 1:
-                    is_HighEnd = True
-                    break
-                elif highEndArgument == 0:
-                    is_HighEnd = False
-                    break
-                else:
-                    print ("Invalid input, please refer to the instructions above")
-            return is_HighEnd
+    def highEndWrite():
 
-        def productionWrite():
-            productionMode = int (input("Is this roll currently in production? Input 1 for yes, 0 for no"))
+        while True:
+            highEndArgument = input("Does one roll of the film cost more than 7 USD?")
 
-            while True:
-                if productionMode == 1 :
-                    is_inProduction = True
-                    break
-                elif productionMode == 0:
-                    is_inProduction = False
-                    break
-                else:
-                    print("Invalid input, please refer to the instructions above")
-            return is_inProduction
+            highEndArgument = checkAgreement(highEndArgument)
 
-        def negOrPos():
-            negorposArg = int (input("""is the roll negative or positive (Slide film)?
-Please input:
-0 for Negative,
-1 for Positive.
-input:"""))
-            if negorposArg == 0:
+            if highEndArgument.isnumeric() and int(highEndArgument) == 1:
+                is_HighEnd = True
+                break
+            elif highEndArgument.isnumeric() and int(highEndArgument) == 0:
+                is_HighEnd = False
+                break
+            else:
+                print ("Invalid input, please refer to the instructions above")
+        return is_HighEnd
+
+    def productionWrite():
+
+        while True:
+            productionMode = input("Is this roll currently in production?")
+
+            productionMode = checkAgreement(productionMode)
+
+            if productionMode.isnumeric() and int(productionMode) == 1 :
+                is_inProduction = True
+                break
+            elif productionMode.isnumeric() and int(productionMode) == 0:
+                is_inProduction = False
+                break
+            else:
+                print("Invalid input, please refer to the instructions above")
+        return is_inProduction
+
+    def negOrPos():
+        while True:
+            negorposArg = input("""is the roll negative?(write No if it's Slide)
+input:""")
+            negorposArg = checkAgreement(negorposArg)
+
+            if negorposArg.isnumeric() and int(negorposArg) == 1:
                 return "Negative"
-            elif negorposArg == 1:
+            elif negorposArg.isnumeric() and int(negorposArg) == 0:
                 return "Slide"
             else:
                 print("Invalid input, please refer to the instructions above")
 
 
-        print("""
+    print("""
 Hello! You are now in Admin mode.
 Here you can add new rolls of film that you found are missing from the database.
 Below are the list of Brands that already exist in the database. \n""")
 
-        print (("   \n").join(RollData.keys()))
+    print (("   \n").join(RollData.keys()))
 
-        brandChoice = brandChoice()
+    brandChoice = brandChoice()
 
-        print ("Here are the models that already exist for the selected brand:\n",)
-        modelsInBrand(brandChoice)
-        modelName = input ("What is the name of the roll you'd like to add")
+    print ("Here are the models that already exist for the selected brand:\n",)
+    modelsInBrand(brandChoice,RollData)
+    modelName = input ("What is the name of the roll you'd like to add")
 
-        ISOValues = ISOWrite()
+    ISOValues = ISOWrite()
 
-        Color = typeWrite()
+    Color = typeWrite()
 
-        usage = usageWrite()
+    usage = usageWrite()
 
-        highend = highEndWrite()
+    highend = highEndWrite()
 
-        type = negOrPos()
-
-
-
-        production = productionWrite()
-
-        newRoll = {
-            "model": modelName,
-            "ISO": ISOValues,
-            "type": type,
-            "Usage": usage,
-            "is_HighEnd": highend,
-            "is_Color": Color,
-            "is_inProduction": production
-        }
-
-        RollData[brandChoice].append(newRoll)
-
-        print ("Roll added succefully!!\n Thank you for your contribution.")
-
-        with open('RollData.json', 'w') as writeData:
-            json.dump(RollData, writeData, indent=4, separators=(',', ': '))
-
-    getRollData()
-
-    modeChoice()
+    type = negOrPos()
 
 
 
+    production = productionWrite()
 
+    newRoll = {
+        "model": modelName,
+        "ISO": ISOValues,
+        "type": type,
+        "Usage": usage,
+        "is_HighEnd": highend,
+        "is_Color": Color,
+        "is_inProduction": production
+    }
+
+    RollData[brandChoice].append(newRoll)
+
+    print ("Roll added succefully!!\n Thank you for your contribution.")
+
+    with open('RollData.json', 'w') as writeData:
+        json.dump(RollData, writeData, indent=4, separators=(',', ': '))
+
+def main ():
+
+    RollData = getRollData()
+
+    print (""" Hello! Welcome to FilmBuddy!
+    
+This application will help you choose a roll of film for your next photo shoot.
+Follow the instructions carefully, and enjoy!
+
+
+""")
+
+    modeChoice(RollData)
 
 main()
